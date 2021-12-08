7efb2da9c929629bb20c6c1cf6f27b36ba3691d6
Helen Kim <rolexvinchester@gmail.com>
2021-12-08 16:27:46 +0300

    blade view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //$product = Product::find(1);
+//    $data = [
+//        'name'=> 'create product',
+//        'price'=> 1000
+//    ];
+//    $product = Product::create($data);
+//    dd($product);
+//    $product = new Product();
+//    $product->name = 'New name 12';
+//    $product->price = 135;
+//    $product->save();
+    $list = Product::
+    //where('status', true)
+         where('price', '>', 10000)
+         ->get();
+    //dd($list);
+    return view('main');
 });
+
+Route::get('/store', function () {
+    return view('store');
+});
+
+Route::get('hello', [SiteController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
