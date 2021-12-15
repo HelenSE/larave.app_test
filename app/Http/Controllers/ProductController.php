b34cc5b911fe9c1b7e9dc8bd066f5122c041ca8b
HelenSE <86287235+HelenSE@users.noreply.github.com>
2021-12-16 00:02:58 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,8 +21,7 @@ class ProductController extends Controller
 
     public function catalog()
     {
-        $products = Product::where('status', 1)
-            ->simplePaginate(9);
+        $products = Product::where('status', 1)->simplePaginate(9);
         $categories = Category::all();
         $brands = Brand::all();
         return view('product.catalog', [
